83dac14c3441ec10a449dd1d22d9d40071d74f08
Farfalak <Farfalak@DESKTOP-US7UPUB>
2024-03-16 15:08:37 +0200

    ServiceCollectionExtension added to clear the program.cs

---
+++ Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            
+
+            return services;
+        }
+
+        public static IServiceCollection AddAplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<HouseRentingDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddScoped<IRepository, Repository>();
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+                    .AddEntityFrameworkStores<HouseRentingDbContext>();
+
+            return services;
+        }
+    }
+}
+}
