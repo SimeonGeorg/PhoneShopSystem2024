2b87d4301882a7b3db74ae701b43fb2904f73921
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-02 13:42:08 +0300

    Added Extensions folder with ServiceCollectionExtension class to be more clear the program.cs file

---
+++ PhoneCatalog/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PhoneCatalog.Infrastructure.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+
+        public static IServiceCollection AddAplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<PhoneCatalogDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            //services.AddScoped<IRepository, Repository>();
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+                    .AddEntityFrameworkStores<PhoneCatalogDbContext>();
+
+            return services;
+        }
+    }
+}
