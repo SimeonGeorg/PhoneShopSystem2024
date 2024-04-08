073631d39161a31459ebd7e690d83757515b885f
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-09 01:19:21 +0300

    Added IPhoneModel interface and AllPhoneService model

---
+++ PhoneCatalog.Core/Contracts/IPhoneModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCatalog.Core.Contracts
+{
+    public interface IPhoneModel
+    {
+        public string Brand { get; set; } 
+        
+        public string Model { get; set; } 
+        
+    }
+}
---
+++ PhoneCatalog.Core/Models/AllPhoneServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using PhoneCatalog.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCatalog.Core.Models
+{
+    public class AllPhoneServiceModel : IPhoneModel
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
