441ab31377476afcd5531a8b14ab2184ee1a008c
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-09 01:18:02 +0300

    IPhoneService interface and PhoneService class added

---
+++ PhoneCatalog.Core/Contracts/IPhoneService.cs
@@ -0,0 +1,14 @@
+﻿using PhoneCatalog.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCatalog.Core.Contracts
+{
+    public interface IPhoneService
+    {
+        Task<IEnumerable<AllPhoneServiceModel>> AllPhonesAsync();
+    }
+}
---
+++ PhoneCatalog.Core/Services/PhoneService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneCatalog.Core.Contracts;
+using PhoneCatalog.Core.Models;
+using PhoneCatalog.Infrastructure.Data.Common;
+using PhoneCatalog.Infrastructure.Data.Models;
+
+namespace PhoneCatalog.Core.Services
+{
+    public class PhoneService : IPhoneService
+    {
+        private readonly IRepository repository;
+        public PhoneService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task<IEnumerable<AllPhoneServiceModel>> AllPhonesAsync()
+        {
+           
+                return await repository
+                    .AllNoTracking<Phone>()
+                    .Select(p => new AllPhoneServiceModel()
+                    {
+                       Id = p.Id,
+                       Brand = p.Brand,
+                       Model = p.Model,
+                       ImageUrl = p.ImageUrl,
+                    })
+                    .ToListAsync();
+            
+        }
+    }
+}
