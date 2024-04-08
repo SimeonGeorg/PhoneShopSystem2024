f2c4236a520307e569d6033ab484a21b46c5d771
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-08 12:13:42 +0300

    Delete From Owner table column Name and Email

---
+++ PhoneCatalog.Infrastructure/Migrations/20240408091034_DeleteColumnsFromOwnerTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhoneCatalog.Infrastructure.Migrations
+{
+    public partial class DeleteColumnsFromOwnerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Owners");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Owners");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Owners",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                comment: "Owner email address");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Owners",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                comment: "Owner  name");
+        }
+    }
+}
