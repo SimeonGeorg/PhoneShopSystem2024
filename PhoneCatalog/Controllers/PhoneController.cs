d97313b55805942cbfcb767cb837e0efb4f6900e
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-10 13:00:47 +0300

    Add phoneController with method All to show all phones

---
+++ PhoneCatalog/Controllers/PhoneController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneCatalog.Core.Contracts;
+
+namespace PhoneCatalog.Controllers
+{
+    public class PhoneController : BaseController
+    {
+        private readonly ILogger<PhoneController> _logger;
+        private readonly IPhoneService phoneService;
+
+        public PhoneController(
+            ILogger<PhoneController> logger,
+            IPhoneService _phoneService)
+        {
+            _logger = logger;
+            phoneService = _phoneService;
+        }
+
+        public async Task<IActionResult> AllAsync()
+        {
+            var model = await phoneService.AllPhonesAsync();
+            return View(model);
+        }
+    }
+}
