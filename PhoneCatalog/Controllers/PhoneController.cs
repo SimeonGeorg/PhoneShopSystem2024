799187e0164e5aa7f851e2fbf683a8d08d046714
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-11 14:27:13 +0300

    Added Mine method for My Phones

--- PhoneCatalog/Controllers/PhoneController.cs
+++ PhoneCatalog/Controllers/PhoneController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PhoneCatalog.Core.Contracts;
+using PhoneCatalog.Core.Models.Phone;
+using PhoneCatalog.Core.Services;
+using System.Security.Claims;
 
 namespace PhoneCatalog.Controllers
 {
@@ -7,18 +10,43 @@ namespace PhoneCatalog.Controllers
     {
         private readonly ILogger<PhoneController> _logger;
         private readonly IPhoneService phoneService;
+        private readonly IOwnerService ownerService;
 
         public PhoneController(
             ILogger<PhoneController> logger,
-            IPhoneService _phoneService)
+            IPhoneService _phoneService,
+            IOwnerService _ownerService)
         {
             _logger = logger;
             phoneService = _phoneService;
+            ownerService = _ownerService;
+
         }
 
-        public async Task<IActionResult> AllAsync()
+        public async Task<IActionResult> All()
         {
             var model = await phoneService.AllPhonesAsync();
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Mine()
+        {
+            var userId = User.Id();
+            IEnumerable<PhoneServiceModel> model;
+
+            
+
+            if (await ownerService.IsExistByIdAsync(userId))
+            {
+                int ownerId = await ownerService.GetOwnerIdAsync(userId) ?? 0;
+                model = await phoneService.AllPhonesByOwnerIdAsync(ownerId);
+                return View(model);
+            }
+            else
+            {
+                model = await phoneService.AllPhonesByUserIdAsync(userId);
+            }
             return View(model);
         }
     }
