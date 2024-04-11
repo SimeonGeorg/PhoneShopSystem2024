7573fa0edcd7984d056a6efaecf6a220bc414743
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-11 14:25:12 +0300

    Added GetOwnerIdAsync method

---
+++ PhoneCatalog.Core/Contracts/IOwnerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCatalog.Core.Contracts
+{
+    public interface IOwnerService
+    {
+        Task<bool> IsExistByIdAsync(string userId);
+        Task<int?> GetOwnerIdAsync(string userId);
+    }
+}
