c20cb8797278ebd1416997b121174922ebd8773e
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-12 12:38:03 +0300

    Implemented 2 methods from the interface for PhoneDetails

--- PhoneCatalog.Core/Services/PhoneService.cs
+++ PhoneCatalog.Core/Services/PhoneService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Logging;
 using PhoneCatalog.Core.Contracts;
+using PhoneCatalog.Core.Models.Performance;
 using PhoneCatalog.Core.Models.Phone;
 using PhoneCatalog.Infrastructure.Data.Common;
 using PhoneCatalog.Infrastructure.Data.Models;
@@ -16,24 +18,24 @@ namespace PhoneCatalog.Core.Services
         }
         public async Task<IEnumerable<AllPhoneServiceModel>> AllPhonesAsync()
         {
-           
-                return await repository
-                    .AllNoTracking<Phone>()
-                    .Select(p => new AllPhoneServiceModel()
-                    {
-                       Id = p.Id,
-                       Brand = p.Brand,
-                       Model = p.Model,
-                       ImageUrl = p.ImageUrl,
-                    })
-                    .ToListAsync();
-            
+
+            return await repository
+                .AllNoTracking<Phone>()
+                .Select(p => new AllPhoneServiceModel()
+                {
+                    Id = p.Id,
+                    Brand = p.Brand,
+                    Model = p.Model,
+                    ImageUrl = p.ImageUrl,
+                })
+                .ToListAsync();
+
         }
 
         public async Task<IEnumerable<PhoneServiceModel>> AllPhonesByOwnerIdAsync(int ownerId)
         {
             return await repository.AllNoTracking<Phone>()
-                
+
                .Where(p => p.OwnerId == ownerId)
                .Select(p => new PhoneServiceModel()
                {
@@ -49,7 +51,7 @@ namespace PhoneCatalog.Core.Services
         public async Task<IEnumerable<PhoneServiceModel>> AllPhonesByUserIdAsync(string userId)
         {
             return await repository.AllNoTracking<Phone>()
-                .Where(p=>p.Owner.UserId == userId)
+                .Where(p => p.Owner.UserId == userId)
                 .Select(p => new PhoneServiceModel()
                 {
                     Id = p.Id,
@@ -60,5 +62,45 @@ namespace PhoneCatalog.Core.Services
                 })
                 .ToListAsync();
         }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await repository.AllNoTracking<Phone>()
+               .AnyAsync(h => h.Id == id);
+        }
+
+        public async Task<PhoneDetailsServiceModel> PhoneDetailsByIdAsync(int id)
+        {
+            return await repository.AllNoTracking<Phone>()
+                .Where(p => p.Id == id)
+                .Select( p => new PhoneDetailsServiceModel()
+                {
+                    Id = p.Id,
+                    Brand = p.Brand,
+                    Model = p.Model,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl,
+                    Category = p.CategoryType.Name,
+                    OwnerPhoneNumber = p.Owner.PhoneNumber,
+                    OwnerId = p.Owner.Id.ToString()
+                })
+                .FirstAsync();
+        }
+        public async Task<PerformanceDetailsModel> PerformanceDetailsByPhoneIdAsync(int id)
+        {
+            return await repository.AllNoTracking<Performance>()
+                .Where (p => p.PhoneId == id)
+                .Select (p => new PerformanceDetailsModel()
+                {
+                    Id = p.Id,
+                    Ram = p.Ram,
+                    Processor = p.Processor,
+                    Battery = p.Battery,
+                    CameraPxl = p.CameraPxl,
+                    Storage = p.Storage,
+                })
+                .FirstAsync();
+        }
     }
 }
+
