7454a5f7586cba1ed60c479cb9c03c94d511ed3d
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-12 12:38:29 +0300

    Added PhoneDetailsServiceModel

---
+++ PhoneCatalog.Core/Models/Phone/PhoneDetailsServiceModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneCatalog.Core.Contracts;
+using PhoneCatalog.Core.Models.Performance;
+using PhoneCatalog.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCatalog.Core.Models.Phone
+{
+    public class PhoneDetailsServiceModel : PhoneServiceModel
+    {
+        public string Category { get; set; } = null!;
+        public string OwnerPhoneNumber { get; set; } = null!;
+        public string OwnerId { get; set; } = null!;
+        public PerformanceDetailsModel Performances { get; set; } = null!;
+
+    }
+}
