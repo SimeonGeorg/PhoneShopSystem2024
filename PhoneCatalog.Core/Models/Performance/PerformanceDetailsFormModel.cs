7a34764291b186be8f4bd14756d1f9c60a3c11d1
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-14 19:01:59 +0300

    Add Performance Details Form model for adding performance to the phone form

---
+++ PhoneCatalog.Core/Models/Performance/PerformanceDetailsFormModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PhoneCatalog.Core.Constants.MessageConstants;
+using static PhoneCatalog.Infrastructure.Constants.DataConstants;
+
+namespace PhoneCatalog.Core.Models.Performance
+{
+    public class PerformanceDetailsFormModel
+    {
+       
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Phone RAM memory")]
+        [StringLength(PhonePerformanceMaxLength
+            ,MinimumLength = PhonePerformanceMinLength
+            ,ErrorMessage = LengthMessage)]
+        public string Ram { get; set; } = null!;
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Phone processor")]
+        [StringLength(PhonePerformanceMaxLength,
+            MinimumLength = PhonePerformanceMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Processor { get; set; } = null!;
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Phone storage")]
+        [StringLength(PhonePerformanceMaxLength,
+            MinimumLength = PhonePerformanceMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Storage { get; set; } = null!;
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Phone camera pixels")]
+        [StringLength(PhonePerformanceMaxLength,
+            MinimumLength = PhonePerformanceMinLength,
+            ErrorMessage = LengthMessage)]
+        public string CameraPxl { get; set; } = null!;
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Phone battery")]
+        [StringLength(PhonePerformanceMaxLength,
+            MinimumLength = PhonePerformanceMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Battery { get; set; } = null!;
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Phone identifier")]
+        public int PhoneId { get; set; }
+
+    }
+}
