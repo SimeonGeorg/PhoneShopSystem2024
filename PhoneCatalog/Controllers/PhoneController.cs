c93412c837b21d015276b7d2aa49c6e3d92d9ff5
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-14 19:02:53 +0300

    Added Add view for phone And implement method in controller phoneController

--- PhoneCatalog/Controllers/PhoneController.cs
+++ PhoneCatalog/Controllers/PhoneController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PhoneCatalog.Core.Contracts;
+using PhoneCatalog.Core.Models.Owner;
 using PhoneCatalog.Core.Models.Phone;
 using PhoneCatalog.Core.Services;
+using PhoneCatalog.Infrastructure.Data.Models;
 using System.Security.Claims;
 
 namespace PhoneCatalog.Controllers
@@ -11,16 +13,20 @@ namespace PhoneCatalog.Controllers
         private readonly ILogger<PhoneController> _logger;
         private readonly IPhoneService phoneService;
         private readonly IOwnerService ownerService;
+        private readonly IPerformanceService performanceService;
       
 
         public PhoneController(
             ILogger<PhoneController> logger,
             IPhoneService _phoneService,
-            IOwnerService _ownerService)
+            IOwnerService _ownerService,
+            IPerformanceService _performanceService)
         {
             _logger = logger;
             phoneService = _phoneService;
             ownerService = _ownerService;
+            performanceService = _performanceService;
+
         }
 
         public async Task<IActionResult> All()
@@ -62,6 +68,39 @@ namespace PhoneCatalog.Controllers
             model.Performances = await phoneService.PerformanceDetailsByPhoneIdAsync(model.Id);
                   
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var modelPhone = new PhoneAddModel()
+            {
+                Categories = await phoneService.AllCategoriesAsync()
+            };
+            return View(modelPhone);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(PhoneAddModel modelPhone)
+        {
+            if (await phoneService.CategoryExistsAsync(modelPhone.CategoryId) == false)
+            {
+                ModelState.AddModelError(nameof(modelPhone.CategoryId), "Category does not exist");
+            }
+            if (ModelState.IsValid == false)
+            {
+                modelPhone.Categories = await phoneService.AllCategoriesAsync();
+
+                return View(modelPhone);
+            }
+
+            int? ownerId = await ownerService.GetOwnerIdAsync(User.Id());         
+
+            int newPhoneId = await phoneService.CreateAsync(modelPhone, ownerId ?? 0);
+
+            int performance = await performanceService.CreateAsync(modelPhone,newPhoneId);
+            
+
+            return RedirectToAction(nameof(Details), new { id = newPhoneId });
         }
 
     }
