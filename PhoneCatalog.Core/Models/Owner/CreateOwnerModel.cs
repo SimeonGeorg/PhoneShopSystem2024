d7b6fca14ce5ff2181cf6ac7ad4e6ab34cca18e3
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-14 19:06:32 +0300

    Create Owner model

---
+++ PhoneCatalog.Core/Models/Owner/CreateOwnerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PhoneCatalog.Core.Constants.MessageConstants;
+using static PhoneCatalog.Infrastructure.Constants.DataConstants;
+
+namespace PhoneCatalog.Core.Models.Owner
+{
+    public class CreateOwnerModel
+    {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(OwnerPhoneNumberMaxLength,
+     MinimumLength = OwnerPhoneNumberMinLength,
+     ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        
+        public string PhoneNumber { get; set; } = null!;
+       
+
+    }
+}
