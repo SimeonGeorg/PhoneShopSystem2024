07f5e508ff30827201c70da73d345d8ea6b4cbe8
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-14 18:59:48 +0300

    Added Comment Service Model

---
+++ PhoneCatalog.Core/Models/Comment/CommentServiceModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneCatalog.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PhoneCatalog.Infrastructure.Constants.DataConstants;
+using static PhoneCatalog.Core.Constants.MessageConstants;
+
+
+namespace PhoneCatalog.Core.Models.Comment
+{
+    public class CommentServiceModel
+    {
+      
+        [Display(Name ="Comment identifier")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Coment text")]
+        [StringLength(CommentMaxLength,
+            MinimumLength = CommentMinLength,
+            ErrorMessage = LengthMessage)]
+        public string CommentText { get; set; } = string.Empty;
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Phone identifier")]
+        public int PhoneId { get; set; }
+        
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Owner identifier")]
+        public int OwnerId { get; set; }
+        
+        
+    }
+}
