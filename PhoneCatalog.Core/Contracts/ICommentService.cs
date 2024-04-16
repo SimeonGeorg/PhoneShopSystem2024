404d475df26936528f677a9f4478546fdb6a0f7e
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-17 00:30:35 +0300

    Added service interface and class for Comments with 2 methods AllCommentByOwnerId, AllCommentByuserId

---
+++ PhoneCatalog.Core/Contracts/ICommentService.cs
@@ -0,0 +1,16 @@
+﻿using PhoneCatalog.Core.Models.Comment;
+using PhoneCatalog.Core.Models.Phone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCatalog.Core.Contracts
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<CommentServiceModel>> AllCommentByOwnerId(int ownerId);
+        Task<IEnumerable<CommentServiceModel>> AllCommentByUserId(string userId);
+    }
+}
---
+++ PhoneCatalog.Core/Services/CommentService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneCatalog.Core.Contracts;
+using PhoneCatalog.Core.Models.Comment;
+using PhoneCatalog.Core.Models.Phone;
+using PhoneCatalog.Infrastructure.Data.Common;
+using PhoneCatalog.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCatalog.Core.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly IRepository repository;
+        public CommentService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task<IEnumerable<CommentServiceModel>> AllCommentByOwnerId(int ownerId)
+        {
+            return await repository.AllNoTracking<Comment>()
+               .Where(c => c.OwnerId == ownerId)
+               .Select(c => new CommentServiceModel()
+               {
+                   CommentText = c.CommentText,
+                   OwnerId = ownerId,
+                   PhoneId = c.PhoneId
+               })
+               .ToListAsync();
+        }
+        public async Task<IEnumerable<CommentServiceModel>> AllCommentByUserId(string userId)
+        {
+            return await repository.AllNoTracking<Comment>()
+                .Where(c => c.Owner.UserId == userId)
+                .Select(c => new CommentServiceModel()
+                {
+                    CommentText = c.CommentText,
+                    OwnerId = c.OwnerId,
+                    PhoneId = c.PhoneId
+                })
+                .ToListAsync();
+        }
+
+    }
+}
