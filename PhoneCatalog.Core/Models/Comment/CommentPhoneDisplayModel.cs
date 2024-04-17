bf64be2acbf6efb800467210770e822a908483f3
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-17 17:06:25 +0300

    Use The CommentPhoneDisplayModel

--- PhoneCatalog.Core/Models/Comment/CommentPhoneDisplayModel.cs
+++ PhoneCatalog.Core/Models/Comment/CommentPhoneDisplayModel.cs
@@ -11,15 +11,11 @@ namespace PhoneCatalog.Core.Models.Comment
 {
     public class CommentPhoneDisplayModel
     {
-        public int Id { get; set; }
-        [Display(Name = "Phone Brand")]
-        public string Brand { get; set; } = string.Empty;
-        [Display(Name = "Phone Model")]
-        public string Model { get; set; } = string.Empty;
-        [Display(Name = "Phone price")]
-        public decimal Price { get; set; }
-        [Display(Name = "Image URL")]
+        
+        [Display(Name = "Phone Image URL")]
         public string ImageUrl { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
 
         public IEnumerable<CommentServiceModel> Comments = new List<CommentServiceModel>();
 
