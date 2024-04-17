5c1abdf620f677ecabf5cd0d9a6da6ba1c392d39
SimeonGeorg <simeon_georgiev97@abv.bg>
2024-04-17 16:17:33 +0300

    use CommentAddModel to add comment to owner and show in Mine the second modelView added but dont use it yet

---
+++ PhoneCatalog.Core/Models/Comment/CommentPhoneDisplayModel.cs
@@ -0,0 +1,27 @@
+﻿using PhoneCatalog.Core.Models.Phone;
+using PhoneCatalog.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCatalog.Core.Models.Comment
+{
+    public class CommentPhoneDisplayModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Phone Brand")]
+        public string Brand { get; set; } = string.Empty;
+        [Display(Name = "Phone Model")]
+        public string Model { get; set; } = string.Empty;
+        [Display(Name = "Phone price")]
+        public decimal Price { get; set; }
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public IEnumerable<CommentServiceModel> Comments = new List<CommentServiceModel>();
+
+    }
+}
